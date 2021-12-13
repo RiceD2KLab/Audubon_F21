a9a5d2afd95f9cbd307c125651c3b83ee63e9c01
krish97 <krish@kabra.com>
2021-12-13 17:27:50 +0300

    add plotting.py

---
+++ utils/plotting.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageDraw
+
+
+def plot_img_bbx(image, annotation_lst, color_dict=None):
+    """
+    This is a plotting function to check if the bndbox annotations convertion is done correctly
+    INPUT:
+    image -- PIL image module to be
+    annotation_lst -- a list containing all the annotations for this image
+    color_dict -- a dictionary containing color scheme to be used to outline bounding boxes for each class
+    """
+    w, h = image.size
+    plotted_img = ImageDraw.Draw(image)
+
+    for annot in annotation_lst:
+        obj_class, desc, x_min, y_min, x_max, y_max = annot
+        if color_dict:
+            plotted_img.rectangle(((x_min, y_min), (x_max, y_max)), width=3, outline=color_dict[obj_class])
+        else:
+            plotted_img.rectangle(((x_min, y_min), (x_max, y_max)), width=3)
+        plotted_img.text((x_min, y_min - 10), obj_class)
+
+    plt.figure(figsize=(60, 30))
+    plt.imshow(np.array(image), interpolation='nearest')
+    plt.show()
