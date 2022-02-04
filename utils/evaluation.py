a287da89819b702804c5acda1dd8cfa1ded43f68
Raul Garcia <rauljgarcia66@gmail.com>
2022-02-04 08:32:14 -0600

    Added confusion matrix

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -11,6 +11,14 @@ from detectron2.evaluation import inference_on_dataset
 from detectron2.evaluation.coco_evaluation import COCOEvaluator
 from detectron2.data import build_detection_test_loader
 
+# import json
+# import torch
+# from torchvision import transforms, datasets
+# from tqdm import tqdm
+from prettytable import PrettyTable
+# from torch import nn
+# import torch.nn.functional as F
+
 
 class PrecisionRecallEvaluator(COCOEvaluator):
     """
@@ -275,4 +283,63 @@ def evaluate_full_pipeline(eval_file_lst, predictor, species_map, raw_img_width,
     return output_df
 
 
+class ConfusionMatrix(object):
+    def __init__(self, num_classes: int, labels: list):
+        self.matrix = np.zeros((num_classes, num_classes))
+        self.num_classes = num_classes
+        self.labels = labels
+    
+    # Update Matrix
+    def update(self, preds, labels):
+        for p, t in zip(preds, labels):
+            self.matrix[p, t] += 1
+    # Show Result(Words)
+    def summary(self):
+        # calculate accuracy
+        sum_TP = 0
+        for i in range(self.num_classes):
+            sum_TP += self.matrix[i, i]
+        acc = sum_TP / np.sum(self.matrix)
+        print("the model accuracy is ", acc)
 
+        # precision, recall, specificity
+        table = PrettyTable()
+        table.field_names = ["", "Precision", "Recall", "Specificity"]
+        for i in range(self.num_classes):
+            TP = self.matrix[i, i]
+            FP = np.sum(self.matrix[i, :]) - TP
+            FN = np.sum(self.matrix[:, i]) - TP
+            TN = np.sum(self.matrix) - TP - FP - FN
+            Precision = round(TP / (TP + FP), 3) if TP + FP != 0 else 0.
+            Recall = round(TP / (TP + FN), 3) if TP + FN != 0 else 0.
+            Specificity = round(TN / (TN + FP), 3) if TN + FP != 0 else 0.
+            table.add_row([self.labels[i], Precision, Recall, Specificity])
+        print(table)
+
+    # Plot
+    def plot(self):
+        matrix = self.matrix
+        # print(matrix)
+        plt.imshow(matrix, cmap=plt.cm.Blues)
+
+        # Set Xlabel
+        plt.xticks(range(self.num_classes), self.labels, rotation=45)
+        # Set Ylabel
+        plt.yticks(range(self.num_classes), self.labels)
+        # Colorbar
+        plt.colorbar()
+        plt.xlabel('True Labels')
+        plt.ylabel('Predicted Labels')
+        plt.title('Confusion matrix')
+
+        # Add classification results into matrix
+        thresh = matrix.max() / 2
+        for x in range(self.num_classes):
+            for y in range(self.num_classes):
+                info = int(matrix[y, x])
+                plt.text(x, y, info,
+                         verticalalignment='center',
+                         horizontalalignment='center',
+                         color="white" if info > thresh else "black")
+        plt.tight_layout()
+        plt.show()
