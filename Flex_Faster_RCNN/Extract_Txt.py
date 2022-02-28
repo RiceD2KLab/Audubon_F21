40668c981aab70e2e52e5ed26c2e03bb14e1f4b9
Maojie Tang <mt84@rice.edu>
2022-02-28 17:41:04 -0600

    faster_rcnn_framework.py: faster R-CNN framework, including convolutional layer, RPN(Working), Data_Generation(Not Finish), ROI Pooling(Not Finish) and Classification(Not Finish) restnet50_fpn: convolutional layer (backbone) feature_region_pyramid: convolutional layer supplement (backbone) transforms.py: data processing for training (data augmentation) text_to_xml.py: Convert a txt file to an xml file in the corresponding format

---
+++ Flex_Faster_RCNN/Extract_Txt.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# class Extract_Txt():
+#     def __init__(self, txt_path):
+#         txt_file =
+
+file = pd.read_csv('/Users/maojietang/Downloads/DJI_20210520122423_0079_03.csv')
