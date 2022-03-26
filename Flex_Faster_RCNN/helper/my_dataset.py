bc2a37aa26fb4f4ecc9587c8dc12514341d11fc6
Maojie Tang <mt84@rice.edu>
2022-03-25 23:03:04 -0500

    Add a new function to compute mean/std for each channel (in order to Normalize)

--- Flex_Faster_RCNN/helper/my_dataset.py
+++ Flex_Faster_RCNN/helper/my_dataset.py
@@ -29,7 +29,7 @@ class VOCDataSet(Dataset):
             assert os.path.exists(xml_path), "not found '{}' file.".format(xml_path)
 
         # read class_indict
-        json_file = './pascal_voc_classes.json'
+        json_file = '/Users/maojietang/Documents/Audubon_F21/Flex_Faster_RCNN/pascal_voc_classes.json'
         assert os.path.exists(json_file), "{} file not exist.".format(json_file)
         json_file = open(json_file, 'r')
         self.class_dict = json.load(json_file)
@@ -182,43 +182,67 @@ class VOCDataSet(Dataset):
     def collate_fn(batch):
         return tuple(zip(*batch))
 
+# computer train_set mean / std for normalization
+def getNormalize(train_data):
+    """
+    Compute mean and variance for training data
+    :param train_data: Dataset(or ImageFolder)
+    :return: (mean, std)
+    """
+    print('Compute mean and variance for training data.')
+    train_loader = torch.utils.data.DataLoader(
+        train_data, batch_size=1, shuffle=False, num_workers=0,
+        pin_memory=True)
+    mean_value = torch.zeros(3)
+    std_value = torch.zeros(3)
+    for X, _ in train_loader:
+        for d in range(3):
+            mean_value[d] += X[:, d, :, :].mean()
+            std_value[d] += X[:, d, :, :].std()
+    mean_value.div_(len(train_data))
+    std_value.div_(len(train_data))
+    print('Computing Complete')
+    return list(mean_value.numpy()), list(std_value.numpy())
 
-# import transforms
-# from draw_box_utils import draw_box
-# from PIL import Image
-# import json
-# import matplotlib.pyplot as plt
-# import torchvision.transforms as ts
-# import random
+
+# if __name__ == '__main__':
+#     import transforms
+#     from draw_box_utils import draw_box
+#     from PIL import Image
+#     import json
+#     import matplotlib.pyplot as plt
+#     import torchvision.transforms as ts
+#     import random
 #
-# # read class_indict
-# category_index = {}
-# try:
-#     json_file = open('/Users/maojietang/Documents/Audubon_F21/Flex_Faster_RCNN/pascal_voc_classes.json', 'r')
-#     class_dict = json.load(json_file)
-#     category_index = {v: k for k, v in class_dict.items()}
-# except Exception as e:
-#     print(e)
-#     exit(-1)
+#     # read class_indict
+#     category_index = {}
+#     try:
+#         json_file = open('/Users/maojietang/Documents/Audubon_F21/Flex_Faster_RCNN/pascal_voc_classes.json', 'r')
+#         class_dict = json.load(json_file)
+#         category_index = {v: k for k, v in class_dict.items()}
+#     except Exception as e:
+#         print(e)
+#         exit(-1)
 #
-# data_transform = {
-#     "train": transforms.Compose([transforms.ToTensor(),
-#                                  transforms.RandomHorizontalFlip(0.5)]),
-#     "val": transforms.Compose([transforms.ToTensor()])
-# }
+#     data_transform = {
+#         "train": transforms.Compose([transforms.ToTensor(),
+#                                      transforms.RandomHorizontalFlip(0.5)]),
+#         "val": transforms.Compose([transforms.ToTensor()])
+#     }
 #
-# # load train data set
-# train_data_set = VOCDataSet('/Users/maojietang/Downloads', "2012", data_transform["train"], "train.txt")
-# print(len(train_data_set))
-# for index in random.sample(range(0, len(train_data_set)), k=5):
-#     img, target = train_data_set[index]
-#     img = ts.ToPILImage()(img)
-#     draw_box(img,
-#              target["boxes"].numpy(),
-#              target["labels"].numpy(),
-#              [1 for i in range(len(target["labels"].numpy()))],
-#              category_index,
-#              thresh=0.5,
-#              line_thickness=5)
-#     plt.imshow(img)
-#     plt.show()
+#     # load train data set
+#     train_data_set = VOCDataSet('/Users/maojietang/Downloads', "2012", data_transform["train"], "train.txt")
+#     print(getNormalize(train_data_set))
+#     print(len(train_data_set))
+#     for index in random.sample(range(0, len(train_data_set)), k=5):
+#         img, target = train_data_set[index]
+#         img = ts.ToPILImage()(img)
+#         draw_box(img,
+#                  target["boxes"].numpy(),
+#                  target["labels"].numpy(),
+#                  [1 for i in range(len(target["labels"].numpy()))],
+#                  category_index,
+#                  thresh=0.5,
+#                  line_thickness=5)
+#         plt.imshow(img)
+#         plt.show()
