db139aeb789751c09498cb666be6a6b6c2fc9d70
Raul Garcia <rauljgarcia66@gmail.com>
2022-03-30 09:51:01 -0500

    Cleaning

--- utils/trainer.py
+++ utils/trainer.py
@@ -8,33 +8,6 @@ from detectron2.data import DatasetMapper, build_detection_test_loader
 from detectron2.engine import DefaultTrainer
 from detectron2.evaluation import COCOEvaluator, DatasetEvaluators
 
-# TODO: error when wandb is not installed on computer. Commenting functions for now
-
-# # TODO: add visualization hook to see visual performance during training
-# class WAndBWriter(EventWriter):
-#     import wandb
-#     """
-#     Write all scalars to a wandb tool.
-#     Code adapted from: https://github.com/facebookresearch/detectron2/issues/774#issuecomment-776944522
-#     """
-
-#     def __init__(self, window_size: int = 20):
-#         self._window_size = window_size
-
-#     def write(self):
-#         storage = get_event_storage()
-#         stats = {}
-#         for k, v in storage.latest_with_smoothing_hint(self._window_size).items():
-#             stats[k.replace("/", "-")] = v[0]
-#         wandb.log(stats, step=storage.iter)
-
-#         if len(storage._vis_data) >= 1:
-#             for img_name, img, step_num in storage._vis_data:\
-#                 wandb.log({img_name: img}, step=step_num)
-
-#     def close(self):
-#         pass
-
 
 class ValidationLossHook(HookBase): 
     """
@@ -125,6 +98,34 @@ class MyTrainer(DefaultTrainer):
             )
         ))
         return hooks
+
+
+# TODO: error when wandb is not installed on computer. Commenting functions for now
+
+# # TODO: add visualization hook to see visual performance during training
+# class WAndBWriter(EventWriter):
+#     import wandb
+#     """
+#     Write all scalars to a wandb tool.
+#     Code adapted from: https://github.com/facebookresearch/detectron2/issues/774#issuecomment-776944522
+#     """
+
+#     def __init__(self, window_size: int = 20):
+#         self._window_size = window_size
+
+#     def write(self):
+#         storage = get_event_storage()
+#         stats = {}
+#         for k, v in storage.latest_with_smoothing_hint(self._window_size).items():
+#             stats[k.replace("/", "-")] = v[0]
+#         wandb.log(stats, step=storage.iter)
+
+#         if len(storage._vis_data) >= 1:
+#             for img_name, img, step_num in storage._vis_data:\
+#                 wandb.log({img_name: img}, step=step_num)
+
+#     def close(self):
+#         pass
 
 
 # class WAndBTrainer(DefaultTrainer):
