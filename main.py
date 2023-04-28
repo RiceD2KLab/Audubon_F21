ed7718099d5d15c1dcc7acd6436faa8391dfe192
Zeyu Yang <zeyuyang8@gmail.com>
2023-04-28 14:46:12 -0500

    update

---
+++ main.py
@@ -0,0 +1,18 @@
+from config import PLOTS_PATH, DESC_MAPPING, ON_DELETE
+from config import OLD_CSV_PATH, NEW_CSV_PATH, TILED_OLD_CSV_PATH, TILED_NEW_CSV_PATH
+from config import DATA_PATH, IMG_PATH, CROPPED_PATH
+from src.data.convert_annotations import write_csv, add_class_id_and_data_exploration
+from src.data.crop_birds import cropping
+
+
+write_csv(OLD_CSV_PATH, NEW_CSV_PATH, DESC_MAPPING, ON_DELETE)
+write_csv(TILED_OLD_CSV_PATH, TILED_NEW_CSV_PATH, DESC_MAPPING, ON_DELETE)
+
+add_class_id_and_data_exploration(NEW_CSV_PATH,
+                                  'histogram of bird species (original images)',
+                                  DATA_PATH, PLOTS_PATH)
+add_class_id_and_data_exploration(TILED_NEW_CSV_PATH,
+                                  'histogram of bird species (tiled images)',
+                                  DATA_PATH, PLOTS_PATH)
+
+cropping(NEW_CSV_PATH, IMG_PATH, CROPPED_PATH)
