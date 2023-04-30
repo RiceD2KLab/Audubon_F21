ac623f55e43d206793cf01d48ba92616461d73d6
Zeyu Yang <zeyuyang8@gmail.com>
2023-04-30 15:42:44 -0500

    doc optimizers

--- src/optimizers/adam.py
+++ src/optimizers/adam.py
@@ -2,7 +2,7 @@ import torch
 
 
 def get_adam_optim(model, lr=0.0001):
-    ''' Return an optimizer associated with the input model '''
+    ''' Return an ADAM optimizer associated with the input model with learning rate lr'''
     params = [param for param in model.parameters() if param.requires_grad]
     optimizer = torch.optim.Adam(params, lr=lr)
     return optimizer
--- src/optimizers/sgd.py
+++ src/optimizers/sgd.py
@@ -2,7 +2,10 @@ import torch
 
 
 def get_sgd_optim(model, lr, momentum=0.9, weight_decay=0.0005):
-    ''' Return an optimizer associated with the input model '''
+    '''
+    Return a SGD optimizer associated with the input model with learning rate lr,
+    momentum momentum and weight decay weight_decay
+    '''
     params = [param for param in model.parameters() if param.requires_grad]
     optimizer = torch.optim.SGD(params, lr=lr, momentum=momentum, weight_decay=weight_decay)
     return optimizer
