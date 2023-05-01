e5a66bb0b6478aedc34c1039dee8552e5c6a0862
ben-daecher <97480387+ben-daecher@users.noreply.github.com>
2023-05-01 12:06:12 -0500

    Update adam.py

--- src/optimizers/adam.py
+++ src/optimizers/adam.py
@@ -6,8 +6,8 @@ def get_adam_optim(model, lr=0.0001):
     Returns an Adam optimizer associated with the input model with learning rate lr
 
     Args:
-        model (nn.Module): PyTorch model to associate the optimizer with
-        lr (float): Learning rate to use for the optimizer (default: 0.0001)
+        model: PyTorch model to associate the optimizer with
+        lr: Learning rate to use for the optimizer (default: 0.0001)
 
     Returns:
         Adam optimizer instance
