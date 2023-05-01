706c7969bba8d2230045266744b6352a22e7586c
ben-daecher <97480387+ben-daecher@users.noreply.github.com>
2023-05-01 12:05:34 -0500

    Update sgd.py

--- src/optimizers/sgd.py
+++ src/optimizers/sgd.py
@@ -3,13 +3,14 @@ import torch
 
 def get_sgd_optim(model, lr, momentum=0.9, weight_decay=0.0005):
     """
-    Returns a stochastic gradient descent (SGD) optimizer associated with the input model, with the specified learning rate, momentum, and weight decay.
+    Returns a stochastic gradient descent (SGD) optimizer associated with the input model, with entered hyperparameters.
+    Used for training the detection and classification models.
 
     Args:
-        model (nn.Module): The PyTorch model for which the optimizer will be created.
-        lr (float): The learning rate for the optimizer.
-        momentum (float, optional): The momentum factor for SGD. Default is 0.9.
-        weight_decay (float, optional): The weight decay (L2 penalty) factor for SGD. Default is 0.0005.
+        model: The PyTorch model for which the optimizer will be created.
+        lr: The learning rate for the optimizer.
+        momentum: The momentum factor for SGD. Default is 0.9.
+        weight_decay: The weight decay (L2 penalty) factor for SGD. Default is 0.0005.
 
     Returns:
         The created SGD optimizer.
