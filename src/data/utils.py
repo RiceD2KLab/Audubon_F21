04ae75df5ce1cfb2e9da9310fdf6dec721ce924d
ben-daecher <97480387+ben-daecher@users.noreply.github.com>
2023-05-01 12:14:57 -0500

    Update utils.py

--- src/data/utils.py
+++ src/data/utils.py
@@ -8,7 +8,7 @@ def csv_to_df(csv_path):
     Reads a CSV file and returns a pandas dataframe.
 
     Args:
-        csv_path (str): The path of the CSV file.
+        csv_path: The path of the CSV file.
 
     Returns:
         A pandas dataframe.
@@ -22,7 +22,7 @@ def concat_frames(file_names):
     Concatenates dataframes from a list of CSV file names into a single pandas dataframe.
 
     Args:
-        file_names (list of str): A list of CSV file names to read and concatenate.
+        file_names: A list of CSV file names to read and concatenate.
 
     Returns:
         A pandas dataframe containing rows from all the concatenated CSV files.
@@ -33,11 +33,12 @@ def concat_frames(file_names):
 
 
 def get_file_names(path, extension):
-    """Returns a sorted list of file names that match the specified file extension in the given directory.
+    """
+    Returns a sorted list of file names that match the specified file extension in the given directory.
 
     Args:
-        path (str): The directory path to search for files.
-        extension (str): The file extension to match. For example, '.csv' for annotations or '.jpg' for images.
+        path: The directory path to search for files.
+        extension: The file extension to match. For example, '.csv' for annotations or '.jpg' for images.
 
     Returns:
         A sorted list of file names that match the specified file extension.
@@ -56,11 +57,11 @@ def split_img_annos(img_files, anno_files, frac, seed=None):
     Splits the image and annotation files into three sets (training, testing, and validation) based on a given fraction.
 
     Args:
-        img_files (list of str): A list of image file paths.
-        anno_files (list of str): A list of annotation file paths.
-        frac (tuple of floats): A tuple of three floats that specifies the fraction of data
+        img_files: A list of image file paths.
+        anno_files: A list of annotation file paths.
+        frac: A tuple of three floats that specifies the fraction of data
             to be used for training, testing, and validation.
-        seed (int or None, optional): A random seed to use for shuffling the files. Defaults to None.
+        seed: A random seed to use for shuffling the files. Defaults to None.
 
     Returns:
         A list of three dictionaries, one for training, testing, and validation respectively,
